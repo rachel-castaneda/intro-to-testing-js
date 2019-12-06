c3728305cb34878af046ae7bfd0ec4d850fb0b95
Rachel Castaneda <rachel.castaneda03@gmail.com>
2019-12-06 10:51:51 -0600

    made another test for returned data type and implemented

--- tests.js
+++ tests.js
@@ -30,5 +30,7 @@ describe('sayHello', function () {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
-    it
+    it('should return a string', function () {
+       expect(typeof sayHello()).toBe('string');
+    });
 });
